39390ea2e22a176f3a37b1cd7ea68de409b1d6d1
israelgal <42181899+israelgal@users.noreply.github.com>
2019-03-29 21:26:06 -0600

    Add files via upload

---
+++ Listas, Condicionales python/Lista de datos personales.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 20:52:48 2019
+
+@author: iisra
+"""
+
+Nombre=input("Escribe tu nombre: ")
+Direccion=input("Escribe tu direccion: ")
+Telefono=input("Escribe tu telefono: ")
+
+Datos=[Nombre, Direccion, Telefono]
+print("Los datos personales son: " + Datos[0] + " " + Datos[1] + " " + Datos[2])
---
+++ Listas, Condicionales python/Tres numeros media aritmetica.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 21:05:09 2019
+
+@author: iisra
+"""
+
+Numero_1=int(input("Escribe un numero: "))
+
+Numero_2=int(input("Escribe un numero: "))
+
+Numero_3=int(input("Escribe un numero: "))
+
+Media_aritmetica=(Numero_1+Numero_2+Numero_3)/3
+
+print("La media aritmetica es: ", Media_aritmetica)
